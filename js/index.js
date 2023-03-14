73be538182c27f1b1a0935880aa22800166280b6
Khalid <114026496+loodi181@users.noreply.github.com>
2023-03-14 19:16:15 +0100

    added some futures

--- js/index.js
+++ js/index.js
@@ -1,20 +1,42 @@
-$(function() {
-    $("input[name='horn-cup']").click(function() {
-      if ($("#horn").is(":checked")) {
-        $("#hornimg").show();
-      } else {
-        $("#hornimg").hide();
-      }
-    });
+// start-Type-of
+$(function () {
+  $("input[name='horn-cup']").click(function () {
+    if ($("#horn").is(":checked")) {
+      $("#hornimg").show();
+    } else {
+      $("#hornimg").hide();
+    }
   });
+});
 
-
-  $(function() {
-    $("input[name='horn-cup']").click(function() {
-      if ($("#cup").is(":checked")) {
-        $("#cupimg").show();
-      } else {
-        $("#cupimg").hide();
-      }
-    });
+$(function () {
+  $("input[name='horn-cup']").click(function () {
+    if ($("#cup").is(":checked")) {
+      $("#cupimg").show();
+    } else {
+      $("#cupimg").hide();
+    }
   });
+});
+
+// end -Type-Of
+
+// start Your Cart
+
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+const output = document.querySelector(".runningTotal");
+
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    const runningTotal =
+      "$" +
+      Array.from(checkboxes)
+        .filter((i) => i.checked) // remove unchecked checkboxes.
+        .map((i) => (i.dataset.amount ??= 0)) //extract the amount, or 0
+        .reduce((total, item) => {
+          return total + parseFloat(item);
+        }, 0);
+    console.log(runningTotal);
+    output.innerHTML = runningTotal;
+  });
+});
