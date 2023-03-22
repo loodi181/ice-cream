ffd7cb0eaa08e60f1cd3f1ccf5d5d3c0533c6293
Khalid <114026496+loodi181@users.noreply.github.com>
2023-03-22 13:45:59 +0100

    update

--- js/index.js
+++ js/index.js
@@ -18,21 +18,23 @@ $(function () {
     }
   });
 });
+// end the type of
 
-// end -Type-Of
 
-// start Your Cart
-
-const checkboxes = document.querySelectorAll("input[type=checkbox]");
+// start the order 
+const inputs = document.querySelectorAll("input[type=number]");
 const output = document.querySelector(".runningTotal");
 
-checkboxes.forEach(function (checkbox) {
-  checkbox.addEventListener("change", function () {
+inputs.forEach(function (input) {
+  input.addEventListener("change", function () {
     const runningTotal =
       "$" +
-      Array.from(checkboxes)
-        .filter((i) => i.checked) // remove unchecked checkboxes.
-        .map((i) => (i.dataset.amount ??= 0)) //extract the amount, or 0
+      Array.from(inputs)
+        .filter((i) => i.value > 0) // Remove inputs with a value of 0 or less.
+        .map((i) => {
+          const amount = i.dataset.amount || 5; // Use the input's data-amount attribute, or default to 5.
+          return i.value * amount; // Multiply each input value by its amount.
+        })
         .reduce((total, item) => {
           return total + parseFloat(item);
         }, 0);
@@ -40,3 +42,19 @@ checkboxes.forEach(function (checkbox) {
     output.innerHTML = runningTotal;
   });
 });
+// end the order
+
+
+// alerting
+let numberInputC = document.getElementById("counterC");
+let numberInputV = document.getElementById("counterV");
+let numberInputS = document.getElementById("counterS");
+let total = 0;
+
+numberInputC.addEventListener("change", function() {
+  total = parseInt(numberInputC.value) +  parseInt(numberInputV.value) + parseInt(numberInputS.value);
+  if (total > 3 ) {
+    alert("Dear Customer, You Can Only Choose 3 Ice-Cream Scoops ;)");
+  }
+});
+
